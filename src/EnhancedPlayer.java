078848a7bd4afacc40d8e2530a05a6fecf7abb21
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-09 07:42:17 +0900

    Update EnhancedPlayer.java

--- src/EnhancedPlayer.java
+++ src/EnhancedPlayer.java
@@ -1,4 +1,45 @@
 package PACKAGE_NAME;
 
 public class EnhancePlayer {
+  private String fullname;
+    private int healthPercentage;
+    private String weapon;
+
+    public EnhancedPlayer(String fullname) {
+//        this.name = name;
+        this(fullname, 100, "Sword");
+    }
+
+    public EnhancedPlayer(String fullname, int healthPercentage, String weapon) {
+        this.fullname = fullname;
+//        this.health = health;
+        if (healthPercentage <= 0 ) {
+            this.healthPercentage = 1;
+        }else if (healthPercentage > 100){
+            this.healthPercentage = 100;
+        } else {
+            this.healthPercentage = healthPercentage;
+        }
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage){
+        healthPercentage -= damage;
+        if(healthPercentage <= 0){
+            System.out.println("Player knocked out of the game");
+        }
+    }
+
+    public int healthRemaining(){
+        return healthPercentage;
+    }
+
+    public void restoreHealth(int extraHealth){
+        healthPercentage += extraHealth;
+        if(healthPercentage > 100){
+            System.out.println("Player restored to 100%");
+            healthPercentage = 100;
+        }
+
+    }
 }
