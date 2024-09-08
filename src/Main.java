4525981f3d7dbd1d554de80d8fd0b344f4172ce6
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-09 07:41:44 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+             System.out.println("OOP - Encapsulation!");
+//        System.out.println("This demonstration isn't the real encapsulation");
+//        Player player = new Player();
+//        player.name ="Jeff";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//        player.health = 200; // not encapsulated properly. direct access to data
+//        player.loseHealth(11);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200,"Sword");
+        System.out.println("Initial health is " + tim.healthRemaining());
     }
 }
